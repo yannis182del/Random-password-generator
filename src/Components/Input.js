2253af0c4e20b268b142817d81ee4c648f00e040
Yannis <yannis.bouacida@appliedtechnology.se>
2020-03-21 20:40:37 +0100

    Minor changes

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
 
 const styles = {
   input: {
@@ -9,27 +10,36 @@ const styles = {
       fontSize: 20,
       fontWeight: 400,
       letterSpacing: "2px",
+      position: "relative"
     },
-    width: "20% ",
-    padding: "0 20px",
+    padding: "10px",
     outline: " 1px #2e2e2e",
     border: " 1px #2e2e2e ",
-    position: "relative",
     minWidth: "300px",
     height: "48px",
-    borderRadius: "10px"
+    borderRadius: "20px"
+  },
+  icon: {
+    fontSize: "60px",
+    cursor: "pointer",
+    color: "white",
+    position: "absolute"
+  },
+  container: {
+    maxWidth: "420px",
+    margin: "auto"
   }
 };
 
 const Input = props => {
   const { classes } = props;
   return (
-    <input
-      className={classes.input}
-      onClick={props.save}
-      placeholder={props.label}
-      disabled
-    />
+    <form>
+      <div className={classes.container}>
+        <input className={classes.input} placeholder={props.label} disabled />
+        <FileCopyIcon onClick={props.save} className={classes.icon} />
+      </div>
+    </form>
   );
 };
 
